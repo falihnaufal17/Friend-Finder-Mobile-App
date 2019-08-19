4939334559c050ef8c807a348a3879fb6ea30dbd
falihnaufal17 <dudu.kotkot@gmail.com>
2019-08-19 11:02:59 +0700

    get current position

--- src/screens/home/Home.js
+++ src/screens/home/Home.js
@@ -2,9 +2,49 @@ import React, { Component } from 'react'
 import { Text, View, StyleSheet, StatusBar } from 'react-native'
 import { Fab, Icon } from 'native-base'
 import MapView, { PROVIDER_GOOGLE, Marker } from 'react-native-maps'
+import GetLocation from 'react-native-get-location'
 
 export default class Home extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            mapRegion: null,
+            latitude: 0,
+            longitude: 0
+        }
+
+
+    }
+    componentDidMount = () => {
+        GetLocation.getCurrentPosition({
+            enableHighAccuracy: true,
+            timeout: 15000,
+        })
+            .then(location => {
+                console.warn(location.latitude);
+
+                let region = {
+                    latitude: location.latitude,
+                    longitude: location.longitude,
+                    latitudeDelta: 0.00922 * 1.5,
+                    longitudeDelta: 0.00421 * 1.5
+                }
+
+                this.setState({
+                    mapRegion: region,
+                    latitude: location.latitude,
+                    longitude: location.longitude
+                })
+            })
+            .catch(error => {
+                const { code, message } = error;
+                console.warn(code, message);
+            })
+    }
+
     render() {
+        console.warn("longitude", this.state.longitude)
+        console.warn("latitude", this.state.latitude)
         return (
             <>
                 <StatusBar translucent={true} backgroundColor="transparent" barStyle="dark-content" />
@@ -14,18 +54,13 @@ export default class Home extends Component {
                         showsMyLocationButton={true}
                         showsIndoorLevelPicker={true}
                         style={styles.map}
-                        initialRegion={{
-                            latitude: -7.7586432,
-                            longitude: 110.3781322,
-                            latitudeDelta: 0.0022,
-                            longitudeDelta: 0.0021,
-                        }}
+                        region={this.state.mapRegion}
                     >
                         <Marker
                             draggable
                             coordinate={{
-                                latitude: -7.7586432,
-                                longitude: 110.3781322,
+                                latitude: this.state.latitude,
+                                longitude: this.state.longitude,
                             }}
                             title="fullname"
                             description="hey i'm here!"
